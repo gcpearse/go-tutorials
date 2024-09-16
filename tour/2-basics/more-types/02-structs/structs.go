6521c5767501b9feac0428b2e2aeb5d98b559293
gcpearse <glen.c.pearse@gmail.com>
2024-09-16 09:23:59 +0100

    Adds reproduced and modified code from the Basics section of A Tour of Go

--- tour/2-basics/more-types/02-structs/structs.go
+++ tour/2-basics/more-types/02-structs/structs.go
@@ -9,10 +9,14 @@ type Vertex struct {
 
 func main() {
 	fmt.Println(Vertex{1, 2})
+
+	v := Vertex{3, 4}
+	v.X = 5
+	fmt.Println(v.X, v.Y)
 }
 
 /*
 
-A struct is a collection of fields.
+A struct is a collection of fields. Struct fields are accessed using dot notation.
 
 */
