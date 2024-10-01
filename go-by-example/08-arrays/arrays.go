f7ce88338d32523c0a2511c78eea26e4bd9b11e3
gcpearse <glen.c.pearse@gmail.com>
2024-10-01 22:02:59 +0100

    Adds reproduced and modified code from Go by Example

---
+++ go-by-example/08-arrays/arrays.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+
+	fmt.Println(a)
+
+	a[4] = 100
+
+	fmt.Println(a)
+	fmt.Println(a[4])
+	fmt.Println(len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+
+	fmt.Println(b)
+
+	b = [...]int{1, 2, 3, 4, 5}
+
+	fmt.Println(b)
+
+	b = [...]int{10, 3: 40, 50}
+
+	fmt.Println(b)
+
+	var nested [2][3]int
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			nested[i][j] = i + j
+		}
+	}
+
+	fmt.Println(nested)
+
+	nested = [2][3]int{
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+
+	fmt.Println(nested)
+}
+
+/*
+
+We can use ... syntax to have the compiler count the number of elements for us.
+
+When an index is specified with :, the elements in between will be zeroed.
+
+*/
