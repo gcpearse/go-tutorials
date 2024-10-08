c76a8e71dc8b0ad5bae029c1b2bc26b987c69907
gcpearse <glen.c.pearse@gmail.com>
2024-10-08 23:34:20 +0100

    Updates examples

--- go-by-example/02-values/values.go
+++ go-by-example/02-values/values.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("port" + "manteau")
 
-	fmt.Println("1 + 1 =", 1+1)
-	fmt.Println("14 / 2.5 =", 14.0/2.5)
+	fmt.Println(1 + 1)
+	fmt.Println(14.0 / 2.5)
 
 	fmt.Println(true && false)
 	fmt.Println(true || false)
